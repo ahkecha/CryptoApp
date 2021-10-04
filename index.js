79fcf4e29079f0756101fa041786b2c07f84d757
akecha <kelgxs@gmail.com>
2021-10-04 18:56:01 +0100

    Useless commit

--- index.js
+++ index.js
@@ -5,5 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
+// need to update this but why would i
 AppRegistry.registerComponent(appName, () => App);
